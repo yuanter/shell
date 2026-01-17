06eeefd274b443a96683f12ee9290e8e55a6feb4
yuanter <1369359503@qq.com>
2026-01-18 01:06:16 +0800

    自动构建flycloud

--- flycloud/docker/docker-entrypoint.sh
+++ flycloud/docker/docker-entrypoint.sh
@@ -23,15 +23,15 @@ fi
 
 echo -e "正在查询最新版本中。。。"
 # 定义版本，根据版本判断
-new_version=$(curl -Ls --connect-timeout 60 "http://ghb.mkjt.xyz:1888/https://raw.githubusercontent.com/yuanter/shell/main/flycloud_beta/version" 2>/dev/null)
+new_version=$(curl -Ls --connect-timeout 60 "http://ghb.mkjt.xyz:1888/https://raw.githubusercontent.com/yuanter/shell/main/flycloud/version" 2>/dev/null)
 if [ -z "$new_version" ]; then
-    new_version=$(curl -Ls --connect-timeout 60 "http://git.566646.xyz:12333/https://raw.githubusercontent.com/yuanter/shell/main/flycloud_beta/version" 2>/dev/null)
+    new_version=$(curl -Ls --connect-timeout 60 "http://git.566646.xyz:12333/https://raw.githubusercontent.com/yuanter/shell/main/flycloud/version" 2>/dev/null)
 fi
 
 new_version=$(echo "$new_version" | tr -d '\r' | tr -d '\n')  # 去掉回车和换行符
 echo -e "[SUCCESS] 当前最新版本为：$(date -d @$((new_version / 1000)) '+%Y-%m-%d %H:%M:%S')"
 
-PID=""
+
 if [ -d "/root/flycloud" ]; then
 	cd /root/flycloud || exit
 	if [ ! -f /root/flycloud/version ]; then
@@ -45,10 +45,10 @@ if [ -d "/root/flycloud" ]; then
 		# 备份旧的应用程序
 		mv /root/flycloud/app.jar /root/flycloud/app.jar.bak
 		# 下载新的应用程序http://ghb.mkjt.xyz:1888/
-		wget -O /root/flycloud/app.jar --timeout=60 --connect-timeout=60 --tries=3 --no-check-certificate http://ghb.mkjt.xyz:1888/https://raw.githubusercontent.com/yuanter/shell/main/flycloud_beta/app.jar > /var/log/app.log 2>&1 || wget -O /root/flycloud/app.jar --timeout=60 --connect-timeout=60 --tries=3 --no-check-certificate http://git.566646.xyz:12333/https://raw.githubusercontent.com/yuanter/shell/main/flycloud_beta/app.jar > /var/log/app.log 2>&1
+		wget -O /root/flycloud/app.jar --timeout=60 --connect-timeout=60 --tries=3 --no-check-certificate http://ghb.mkjt.xyz:1888/https://raw.githubusercontent.com/yuanter/shell/main/flycloud/app.jar > /var/log/app.log 2>&1 || wget -O /root/flycloud/app.jar --timeout=60 --connect-timeout=60 --tries=3 --no-check-certificate http://git.566646.xyz:12333/https://raw.githubusercontent.com/yuanter/shell/main/flycloud/app.jar > /var/log/app.log 2>&1
 		# 成功后下载version文件到本地
-		wget -O /root/flycloud/version --timeout=60 --connect-timeout=60 --tries=3 --no-check-certificate http://ghb.mkjt.xyz:1888/https://raw.githubusercontent.com/yuanter/shell/main/flycloud_beta/version > /var/log/app.log 2>&1 || wget -O /root/flycloud/version --timeout=60 --connect-timeout=60 --tries=3 --no-check-certificate http://git.566646.xyz:12333/https://raw.githubusercontent.com/yuanter/shell/main/flycloud_beta/version > /var/log/app.log 2>&1
-		echo "更新完成，重启应用程序中，请稍候..."
+		wget -O /root/flycloud/version --timeout=60 --connect-timeout=60 --tries=3 --no-check-certificate http://ghb.mkjt.xyz:1888/https://raw.githubusercontent.com/yuanter/shell/main/flycloud/version > /var/log/app.log 2>&1 || wget -O /root/flycloud/version --timeout=60 --connect-timeout=60 --tries=3 --no-check-certificate http://git.566646.xyz:12333/https://raw.githubusercontent.com/yuanter/shell/main/flycloud/version > /var/log/app.log 2>&1
+		echo "更新完成，重启应用程序中，请稍候..." 
 		# 停止应用程序
 		PID=$(pgrep -f "java") && if [ -n "$PID" ]; then kill -9 $PID && echo "停止应用程序java..."; fi
 	else
@@ -56,9 +56,8 @@ if [ -d "/root/flycloud" ]; then
 	fi
 else
 	# 成功后下载version文件到本地
-	wget -O /root/flycloud/version --timeout=60 --connect-timeout=60 --tries=3  --no-check-certificate http://ghb.mkjt.xyz:1888/https://raw.githubusercontent.com/yuanter/shell/main/flycloud_beta/version > /var/log/app.log 2>&1 || wget -O /root/flycloud/version --timeout=60 --connect-timeout=60 --tries=3  --no-check-certificate http://git.566646.xyz:12333/https://raw.githubusercontent.com/yuanter/shell/main/flycloud_beta/version > /var/log/app.log 2>&1
+	wget -O /root/flycloud/version --timeout=60 --connect-timeout=60 --tries=3  --no-check-certificate http://ghb.mkjt.xyz:1888/https://raw.githubusercontent.com/yuanter/shell/main/flycloud/version > /var/log/app.log 2>&1 || wget -O /root/flycloud/version --timeout=60 --connect-timeout=60 --tries=3  --no-check-certificate http://git.566646.xyz:12333/https://raw.githubusercontent.com/yuanter/shell/main/flycloud/version > /var/log/app.log 2>&1
 fi
-
 
 
 if [ -z "$(pgrep -f "java")" ]; then
